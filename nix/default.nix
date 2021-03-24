fb6f9f4e14378e832f9881cb8398282ec9336139
Andrew Hobson <ahobson@truss.works>
2021-03-24 09:14:20 -0400

    Need awscli2

--- nix/default.nix
+++ nix/default.nix
@@ -15,6 +15,14 @@ in buildEnv {
 
     (import (builtins.fetchGit {
       # Descriptive name to make the store path easier to identify
+      name = "awscli2-2.1.7";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "559cf76fa3642106d9f23c9e845baf4d354be682";
+    }) {}).awscli2
+
+    (import (builtins.fetchGit {
+      # Descriptive name to make the store path easier to identify
       name = "nodejs-12.16.3";
       url = "https://github.com/NixOS/nixpkgs/";
       ref = "refs/heads/nixpkgs-unstable";
