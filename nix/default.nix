f5f9ff97d69a76aae4ac99a0130496950a424f62
Andrew Hobson <ahobson@truss.works>
2021-05-04 13:43:52 -0400

    Update nix config

--- nix/default.nix
+++ nix/default.nix
@@ -23,6 +23,14 @@ in buildEnv {
 
     (import (builtins.fetchGit {
       # Descriptive name to make the store path easier to identify
+      name = "aws-vault-6.2.0";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "559cf76fa3642106d9f23c9e845baf4d354be682";
+    }) {}).aws-vault
+
+    (import (builtins.fetchGit {
+      # Descriptive name to make the store path easier to identify
       name = "nodejs-12.16.3";
       url = "https://github.com/NixOS/nixpkgs/";
       ref = "refs/heads/nixpkgs-unstable";
